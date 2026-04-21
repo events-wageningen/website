ed9ec38f48bddad21a7c721dd1dbcb43f2e2c2b1
d-consoli <consoli.it@gmail.com>
2026-04-21 22:46:49 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://events-wageningen.github.io",
+  site: "https://events-wageningen.nl",
   base: "/website",
   output: "static",
   integrations: [tailwind()],
